ddc690def7e3459c50b3e3f20243d27b511f78c9
Victor Mota Mazzei <mazzei92@gmail.com>
2021-07-29 23:23:13 -0300

    Variables
    Adjust some variables

--- src/memory.rs
+++ src/memory.rs
@@ -2,9 +2,11 @@
 
 pub struct Memory {
     // the memory have 4096 bytes
-    ram:[u8;4096]
+    ram: [u8;4096]
     // stack have array of 16 16-bit values
-    stack:[u16;16]
+    stack: [u16;16],
+    // Stack pointer
+    sp: u8
 }
 
 impl Memory {
--- src/processor.rs
+++ src/processor.rs
@@ -3,10 +3,8 @@
 pub struct Processor {
     //Program counter
     pc: u16,
-    //Stack counter
-    sp: u8,
     //General purpose register
-    v:[u8;16]
+    v: [u8;16]
     //Delay timer
     delay_timer: u8,
     //Sound timer
