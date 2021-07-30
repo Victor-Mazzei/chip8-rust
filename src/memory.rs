1f91c09651f03e8d1484b537b93fafe047ecfbd2
Victor Mota Mazzei <mazzei92@gmail.com>
2021-07-29 23:05:13 -0300

    Add Variables
    Start to adding some basics variables for processor and memory

--- src/memory.rs
+++ src/memory.rs
@@ -1,7 +1,10 @@
 /// This struct represents the memory of chip-8
 
 pub struct Memory {
-
+    // the memory have 4096 bytes
+    ram:[u8;4096]
+    // stack have array of 16 16-bit values
+    stack:[u16;16]
 }
 
 impl Memory {
--- src/processor.rs
+++ src/processor.rs
@@ -1,12 +1,22 @@
 /// this struct represents the processor of chip-8
 pub struct Processor {
+    //Program counter
+    pc: u16,
+    //Stack counter
+    sp: u8,
+    //General purpose register
+    v:[u8;16]
+    //Delay timer
+    delay_timer: u8,
+    //Sound timer
+    sound_timer: u8,
 
 }
 
 impl Processor {
     fn new () -> self {
         self {
-            
+
         }
     }
 }
