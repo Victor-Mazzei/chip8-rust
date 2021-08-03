f5ac76bf695df7f59170ffb9819e949a32e36a46
Victor Mota Mazzei <mazzei92@gmail.com>
2021-08-02 21:11:40 -0300

    Keyboard
    Add Keyboard

--- src/chip8.rs
+++ src/chip8.rs
@@ -1,18 +1,20 @@
 /// this struct is responsible for binding all the chip8 functionalities 
 
-use super::{cpu::Cpu, memory::Memory};
+use super::{cpu::Cpu, memory::Memory, keyboard::Keyboard};
 #[derive(Debug)]
 pub struct Chip8 {
     pub memory: Memory,
     pub cpu: Cpu,
+    pub keyboard: Keyboard
 }
 
 impl Chip8 {
-    pub fn new (cpu: Cpu, memory: Memory) -> Self {
+    pub fn new (cpu: Cpu, memory: Memory, keyboard: Keyboard) -> Self {
 
         Self {
             cpu,
             memory,
+            keyboard
         }
     } 
 
--- src/main.rs
+++ src/main.rs
@@ -6,51 +6,55 @@ use sdl2::keyboard::Keycode;
 use chip8::Chip8;
 use cpu::Cpu;
 use memory::Memory;
+use keyboard::Keyboard;
 
 use std::{thread, time};
 
 mod chip8;
 mod cpu;
 mod memory;
-
+mod keyboard;
 
 fn main() {
     //
-    //let mut chip8 = Chip8::new(Cpu::new(),Memory::new());
+    let mut chip8 = Chip8::new(Cpu::new(),Memory::new(),Keyboard::new());
 
-    let sdl_context = sdl2::init().unwrap();
-    let video_subsystem = sdl_context.video().unwrap();
 
-    let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
-        .position_centered()
-        .build()
-        .unwrap();
+    println!("{:?}", chip8);
 
-    let mut canvas = window.into_canvas().build().unwrap();
+    // let sdl_context = sdl2::init().unwrap();
+    // let video_subsystem = sdl_context.video().unwrap();
 
-    canvas.set_draw_color(Color::RGB(0, 255, 255));
-    canvas.clear();
-    canvas.present();
-    let mut event_pump = sdl_context.event_pump().unwrap();
-    let mut i = 0;
-    'running: loop {
-        i = (i + 1) % 255;
-        canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
-        canvas.clear();
-        for event in event_pump.poll_iter() {
-            match event {
-                Event::Quit {..} |
-                Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
-                    break 'running
-                },
-                _ => {}
-            }
-        }
-        // The rest of the game loop goes here...
+    // let window = video_subsystem.window("rust-sdl2 demo", 800, 600)
+    //     .position_centered()
+    //     .build()
+    //     .unwrap();
 
-        canvas.present();
-        thread::sleep(time::Duration::from_millis(100));
-    }
+    // let mut canvas = window.into_canvas().build().unwrap();
+
+    // canvas.set_draw_color(Color::RGB(0, 255, 255));
+    // canvas.clear();
+    // canvas.present();
+    // let mut event_pump = sdl_context.event_pump().unwrap();
+    // let mut i = 0;
+    // 'running: loop {
+    //     i = (i + 1) % 255;
+    //     canvas.set_draw_color(Color::RGB(i, 64, 255 - i));
+    //     canvas.clear();
+    //     for event in event_pump.poll_iter() {
+    //         match event {
+    //             Event::Quit {..} |
+    //             Event::KeyDown { keycode: Some(Keycode::Escape), .. } => {
+    //                 break 'running
+    //             },
+    //             _ => {}
+    //         }
+    //     }
+    //     // The rest of the game loop goes here...
+
+    //     canvas.present();
+    //     thread::sleep(time::Duration::from_millis(100));
+    // }
 
     
 
